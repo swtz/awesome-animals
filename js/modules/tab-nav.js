abbb9ee50b92969a9ccc01f78646a894abaa87ec
Leonardo Lüders <luderser@hotmail.com>
2024-12-26 17:39:09 -0300

    split keyframe 'show-text' in two keyframes 'show-right' and 'show-down' for tab navigation

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,22 +1,21 @@
 export default function initTabNavigation() {
+  const imgList = document.querySelector('.animais-lista');
+  const imgs = imgList.querySelectorAll('li img');
 
-}
+  const sectionList = document.querySelector('.animais-descricao');
+  const sections = sectionList.querySelectorAll('[data-tab-nav]');
+  sections[0].classList.add('ativo');
 
-const imgList = document.querySelector('.animais-lista');
-const imgsLi = imgList.querySelectorAll('li img');
-
-const sectionList = document.querySelector('.animais-descricao');
-const sections = sectionList.querySelectorAll('[data-tab-menu]');
-
-imgsLi.forEach((img) => {
-  img.addEventListener('click', handleClick);
-})
-
-function handleClick(event) {
-  imgsLi.forEach((img, index) => {
-    sections[index].classList.remove('ativo');
-    if (event.target === img) {
-      sections[index].classList.add('ativo');
-    }
+  imgs.forEach((img) => {
+    img.addEventListener('click', handleClick);
   })
+
+  function handleClick(event) {
+    imgs.forEach((img, index) => {
+      sections[index].classList.remove('ativo');
+      if (event.target === img) {
+        sections[index].classList.add('ativo');
+      }
+    })
+  }
 }
