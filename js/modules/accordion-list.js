56878aec23fc2f1dee4b620289a2bc08f1f38e4a
Leonardo Lüders <luderser@hotmail.com>
2024-12-27 11:03:01 -0300

    change 'event.currentTarget' to 'this' | setting class 'active' on first accordion dt & dd

--- js/modules/accordion-list.js
+++ js/modules/accordion-list.js
@@ -2,10 +2,12 @@ export default function initAccordionList() {
   const faqListDt = document.querySelectorAll('[data-accordion] dt');
 
   if (faqListDt.length) {
-    function handleClick(event) {
-      const dd = event.currentTarget.nextElementSibling;
-      event.target.classList.toggle('active');
-      dd.classList.toggle('active');
+    faqListDt[0].classList.add('active');
+    faqListDt[0].nextElementSibling.classList.add('active');
+
+    function handleClick() {
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('active');
     }
 
     faqListDt.forEach(dt => {
