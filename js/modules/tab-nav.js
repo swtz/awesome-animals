68960d3a25c3af87b4ce3d5444f66fe3dddfa591
Leonardo Lüders <luderser@hotmail.com>
2024-12-27 08:25:08 -0300

    added compatibility for non-active-js browsers

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,21 +1,25 @@
 export default function initTabNavigation() {
   const imgList = document.querySelector('.animais-lista');
   const imgs = imgList.querySelectorAll('li img');
-
   const sectionList = document.querySelector('.animais-descricao');
   const sections = sectionList.querySelectorAll('[data-tab-nav]');
-  sections[0].classList.add('ativo');
 
-  imgs.forEach((img) => {
-    img.addEventListener('click', handleClick);
-  })
+  if (!sections.length) {
+    document.documentElement.classList.remove('js');
+  } else {
+    sections[0].classList.add('ativo');
 
-  function handleClick(event) {
-    imgs.forEach((img, index) => {
-      sections[index].classList.remove('ativo');
-      if (event.target === img) {
-        sections[index].classList.add('ativo');
-      }
+    imgs.forEach((img) => {
+      img.addEventListener('click', handleClick);
     })
+
+    function handleClick(event) {
+      imgs.forEach((img, index) => {
+        sections[index].classList.remove('ativo');
+        if (event.target === img) {
+          sections[index].classList.add('ativo');
+        }
+      })
+    }
   }
 }
