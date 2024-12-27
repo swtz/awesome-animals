1599d2aaaec857cb675de94fd2e454c7495244c6
Leonardo Lüders <luderser@hotmail.com>
2024-12-27 11:44:43 -0300

    smooth scroll w/ javascript by myself | attemp: 01

---
+++ js/modules/smooth-scroll.js
@@ -0,0 +1,25 @@
+export default function initSmoothScroll() {
+  const internalLinks = document.querySelectorAll('a[href^="#"]');
+
+  function handleClick(event) {
+    event.preventDefault();
+    const section = findById(event.target);
+    if (section) {
+      const sectionRect = section.getBoundingClientRect();
+      scrollTo(
+        {
+          top: sectionRect.top,
+          behavior: 'smooth'
+        }
+      );
+    }
+  }
+
+  function findById(el) {
+    return document.querySelector(`${el.getAttribute('href')}`);
+  }
+
+  internalLinks.forEach(link => {
+    link.addEventListener('click', handleClick)
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initTabNavigation from "./modules/tab-nav.js";
 import initAccordionList from "./modules/accordion-list.js";
+import initSmoothScroll from "./modules/smooth-scroll.js";
 
 initTabNavigation();
 initAccordionList();
+initSmoothScroll();
