7b8f0dda69e252a8ad094f94343e5717d0e52563
Leonardo Lüders <luderser@hotmail.com>
2024-12-28 10:52:24 -0300

    feature: show modal on click 'login' menu item

---
+++ js/modules/modal.js
@@ -0,0 +1,25 @@
+export default function initModal() {
+  const btnOpen = document.querySelector('[data-modal="open"]');
+  const btnClose = document.querySelector('[data-modal="close"]');
+  const btnLogin = document.querySelector('button[type="submit"]')
+  const modal = document.querySelector('[data-modal="container"]')
+  const events = ['click', 'touchstart'];
+
+  events.forEach(userEvent => {
+    btnOpen.addEventListener(userEvent, handleEvent)
+    btnClose.addEventListener(userEvent, handleEvent)
+    modal.addEventListener(userEvent, clickOutside)
+    btnLogin.addEventListener(userEvent, event => event.preventDefault())
+  })
+
+  function handleEvent(event) {
+    event.preventDefault();
+    modal.classList.toggle('active');
+  }
+
+  function clickOutside(event) {
+    if (this === event.target) {
+      modal.classList.remove('active');
+    }
+  }
+}
--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabNavigation from "./modules/tab-nav.js";
 import initAccordionList from "./modules/accordion-list.js";
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initScrollAnimated from "./modules/scroll-animated.js";
+import initModal from "./modules/modal.js";
 
 initTabNavigation();
 initAccordionList();
 initSmoothScroll();
 initScrollAnimated();
+initModal();
