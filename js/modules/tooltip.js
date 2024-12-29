a1e54c510c009977e86f72e958354fb8ec432ee8
Leonardo Lüders <luderser@hotmail.com>
2024-12-29 10:56:18 -0300

    feature: tooltip box

---
+++ js/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function initTooltip() {
+  const tooltips = document.querySelectorAll('[data-tooltip]');
+
+  if (tooltips) {
+    tooltips.forEach(tooltip => {
+      tooltip.addEventListener('mouseover', onMouseOver);
+    })
+  }
+
+  function onMouseOver() {
+    const tooltipBox = createTooltipBox(this);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+  }
+
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = `${event.pageY + 15}px`;
+      this.tooltipBox.style.left = `${event.pageX + 15}px`;
+    }
+  }
+
+  const onMouseLeave = {
+    handleEvent() {
+      this.tooltipBox.remove();
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+      this.element.removeEventListener('mousemove', onMouseMove);
+    }
+  }
+
+  function createTooltipBox(element) {
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordionList from "./modules/accordion-list.js";
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initScrollAnimated from "./modules/scroll-animated.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabNavigation();
 initAccordionList();
 initSmoothScroll();
 initScrollAnimated();
 initModal();
+initTooltip();
