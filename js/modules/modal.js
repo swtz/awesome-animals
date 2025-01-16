8e7e6e81b943efea6b6716dc9fce0e4f3c2b7b56
Leonardo Lüders <luderser@hotmail.com>
2025-01-16 11:08:26 -0300

    module modal refactored | transformed in a class for reuse in any website built with javascript

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,25 +1,57 @@
-export default function initModal() {
-  const btnOpen = document.querySelector('[data-modal="open"]');
-  const btnClose = document.querySelector('[data-modal="close"]');
-  const btnLogin = document.querySelector('button[type="submit"]');
-  const modal = document.querySelector('[data-modal="container"]');
-  const events = ['click', 'touchstart'];
+import { events } from './utils/utils';
 
-  function handleEvent(event) {
-    event.preventDefault();
-    modal.classList.toggle('active');
+export default class Modal {
+  constructor(buttonOpen, buttonClose, containerModal, buttonLogin, className) {
+    this.btnOpen = document.querySelector(buttonOpen);
+    this.btnClose = document.querySelector(buttonClose);
+    this.containerModal = document.querySelector(containerModal);
+    this.btnLogin = document.querySelector(buttonLogin);
+    this.activeClass = className;
+    this.events = events;
+
+    // binding 'this' to the callback to reference the class' object
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clickOutsideModal = this.clickOutsideModal.bind(this);
   }
 
-  function clickOutside(event) {
-    if (this === event.target) {
-      modal.classList.remove('active');
+  // open/close the modal
+  toggleModal() {
+    this.containerModal.classList.toggle(this.activeClass);
+  }
+
+  // handle the 'userEvent' from 'addModalEvent'
+  // calls 'toggleModal' to show the modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  // adds the mobile pattern 'click outside' on the modal
+  clickOutsideModal(event) {
+    if (this.containerModal === event.target) {
+      this.containerModal.classList.remove(this.activeClass);
     }
   }
 
-  events.forEach((userEvent) => {
-    btnOpen.addEventListener(userEvent, handleEvent);
-    btnClose.addEventListener(userEvent, handleEvent);
-    modal.addEventListener(userEvent, clickOutside);
-    btnLogin.addEventListener(userEvent, (event) => event.preventDefault());
-  });
+  // prevents the default behavior of submit button
+  preventBtnSubmitDefault() {
+    this.events.forEach((userEvent) => {
+      this.btnLogin.addEventListener(userEvent, (event) => event.preventDefault());
+    });
+  }
+
+  // adds events on selected buttons and modal's container
+  addModalEvent() {
+    this.events.forEach((userEvent) => {
+      this.btnOpen.addEventListener(userEvent, this.eventToggleModal);
+      this.btnClose.addEventListener(userEvent, this.eventToggleModal);
+      this.containerModal.addEventListener(userEvent, this.clickOutsideModal);
+    });
+  }
+
+  init() {
+    if (this.btnOpen && this.btnClose && this.containerModal) this.addModalEvent();
+    if (this.btnLogin) this.preventBtnSubmitDefault();
+    return this;
+  }
 }
