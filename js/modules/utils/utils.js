19574e4af8d4d26fa60828cf9333a823c8651c0d
Leonardo Lüders <luderser@hotmail.com>
2025-01-21 11:11:42 -0300

    created 'debounce' function on 'utils' package/module.

--- js/modules/utils/utils.js
+++ js/modules/utils/utils.js
@@ -1,2 +1,14 @@
 export const isMobile = matchMedia('(max-width: 700px)').matches;
 export const events = ['touchstart', 'click'];
+
+export default function debounce(callback, delay) {
+  let timer;
+
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+      timer = null;
+    }, delay);
+  };
+}
