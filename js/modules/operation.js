2b231e26d7966c52d4b8f9f586f01de605d2b56a
Leonardo Lüders <luderser@hotmail.com>
2025-01-22 10:51:18 -0300

    operation module refactoring done.

--- js/modules/operation.js
+++ js/modules/operation.js
@@ -6,15 +6,20 @@ export default class Operation {
     this.activeClass = className;
   }
 
+  // gets current date and creates 'now' property.
   setCurrentDate() {
     this.now = new Date();
   }
 
+  // transforms 'weekDays' and 'weekHours' in array of numbers.
   setOperation() {
     this.operationDays = this.weekDays.split(',').map(Number);
     this.operationHours = this.weekHours.split(',').map(Number);
   }
 
+  // checks if current hour is within the operation hours.
+  // checks if current day is within the operation days
+  // and return the boolean result with 'AND' operator.
   isOperating() {
     const isOperationHour = (
       this.now.getHours() >= this.operationHours[0]
@@ -25,6 +30,8 @@ export default class Operation {
     return (isOperationDay && isOperationHour);
   }
 
+  // if 'isOperating' returns 'true', add the class
+  // to the 'this.operation' pseudo-element.
   setOperationStatus() {
     if (this.isOperating()) this.operation.classList.add(this.activeClass);
   }
